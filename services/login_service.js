6917c68f48d03c7b2b9254e20a34f0e2ea0b3120
Augusto Queirantes <augusto_queirantes@outlook.com>
2019-06-02 00:52:00 -0300

    Improve login service

--- services/login_service.js
+++ services/login_service.js
@@ -4,5 +4,14 @@ export default {
     localStorage.setItem('name', profile.getName());
     localStorage.setItem('imageUrl', profile.getImageUrl());
     localStorage.setItem('email', profile.getEmail());
+  },
+
+  isSignedIn() {
+    googleId = LocalStorage.getItem('googleId');
+    name = LocalStorage.getItem('name');
+    imageUrl = LocalStorage.getItem('imageUrl');
+    email = LocalStorage.getItem('email');
+
+    return googleId.length() > 0 && name.length() > 0 && imageUrl.length() > 0 && email.length() > 0;
   }
 }

7788a08f06675b754b79d07097fcb06c5dccdcfd
Augusto Queirantes <augusto_queirantes@outlook.com>
2019-06-02 01:44:13 -0300

    Add courses page template

---
+++ pages/courses/new.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    That's new course page
+    <vue-bootstrap-typeahead
+      v-model="query"
+      :data="['Canada', 'USA', 'Mexico']"
+    />
+  </div>
+</template>
+
+<script>
+import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
+
+export default {
+  componenets: {
+    VueBootstrapTypeahead
+  },
+  data() {
+    return {
+      query: '',
+    }
+  },
+}
+</script>
